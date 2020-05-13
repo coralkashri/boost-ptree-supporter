2979f74a0876255173d7254e3e4a5e3c24f25849
Korel Kashri <korelkashri@gmail.com>
2020-05-14 02:19:32 +0300

    Add some string casting

---
+++ src/my_property_tree.cpp
@@ -0,0 +1,5 @@
+//
+// Created by sherlock on 5/14/20.
+//
+
+#include "my_property_tree.h"
---
+++ src/my_property_tree.h
@@ -0,0 +1,16 @@
+//
+// Created by sherlock on 5/14/20.
+//
+
+#ifndef BOOSTPTREESUPPORTER_MY_PROPERTY_TREE_H
+#define BOOSTPTREESUPPORTER_MY_PROPERTY_TREE_H
+
+
+
+class my_property_tree {
+
+};
+
+
+
+#endif //BOOSTPTREESUPPORTER_MY_PROPERTY_TREE_H
